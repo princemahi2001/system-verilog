5621434888aceabf042efb0d2672b8f435f937e8
princemahi2001 <94538166+princemahi2001@users.noreply.github.com>
2023-08-10 15:15:55 +0530

    Create fork-join.sv

---
+++ Processes/fork-join.sv
@@ -0,0 +1,13 @@
+module tb;
+  initial begin
+    fork
+      $display("fork1 called at %0d",$time);
+      #2 $display("fork2 called at %0d",$time);
+      #2 $display("fork3 called at %0d",$time);
+      #5 $display("fork4 called at %0d",$time);
+      #8 $display("fork5 called at %0d",$time);
+      #9 $display("fork6 called at %0d",$time);
+    join
+    #2 $display("out fork called at %0d",$time);
+  end
+endmodule

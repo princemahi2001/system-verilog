5be5467ef512d2da6084e1dc256ca96531bbdb00
princemahi2001 <94538166+princemahi2001@users.noreply.github.com>
2023-08-14 13:00:22 +0530

    Create use of super keyword.sv

---
+++ Class/use of super keyword.sv
@@ -0,0 +1,24 @@
+class parent;
+  int a;
+  function void display();
+    $display("a value is %0d",a);
+  endfunction
+endclass
+
+class child1 extends parent;
+  int b;
+  function void display();
+    super.display();
+    $display("b value is %0d",b);
+  endfunction
+endclass
+
+module tb;
+  child1 c1;
+  initial begin
+    c1=new();
+    c1.a=3;
+    c1.b=5;
+    c1.display();
+  end
+endmodule

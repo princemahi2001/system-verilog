a2ce05d9a6301f90ec6b2e80f51d4b71c598c5f5
princemahi2001 <94538166+princemahi2001@users.noreply.github.com>
2023-08-14 13:06:50 +0530

    Update use of super keyword.sv

--- Class/use of super keyword.sv
+++ Class/use of super keyword.sv
@@ -22,3 +22,36 @@ module tb;
     c1.display();
   end
 endmodule
+
+//output
+a value is 3
+b value is 5
+
+//example with arguments in the constructors
+class parent;
+  int a;
+  function new(int a);
+    this.a=a;
+    $display("a value is %0d",a);
+  endfunction
+endclass
+
+class child1 extends parent;
+  int b;
+  function new(int a,int b);
+    super.new(a);
+    $display("b value is %0d",b);
+  endfunction
+endclass
+
+module tb;
+  child1 c1;
+  initial begin
+    c1=new(3,5);
+  end
+endmodule
+
+
+//output
+a value is 3
+b value is 5

f5689520a85fd2c553c993309bbfab72971fd860
princemahi2001 <94538166+princemahi2001@users.noreply.github.com>
2023-08-14 12:30:47 +0530

    Update single inheritance.sv

--- Class/Inheritance/single inheritance.sv
+++ Class/Inheritance/single inheritance.sv
@@ -1,12 +1,12 @@
 class parent;
   int a;
-  function void display();
+  function void display1();
     $display("a value is %0d",a);
   endfunction
 endclass
 class child extends parent;
   int b;
-  function void display();
+  function void display2();
     $display("b value is %0d",b);
   endfunction
 endclass
@@ -15,9 +15,10 @@ module tb;
   parent p1;
   initial begin
     c1=new();
+    //p1=new();
     c1.a=3;
     c1.b=5;
-    c1.display();
-    p1.display();
+    c1.display1();
+    c1.display2();
   end
 endmodule
